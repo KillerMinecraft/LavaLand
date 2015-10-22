c3613076c0bf4ef9e7802bf40e1dbd428c1eea0e
FTWinston <ftwinston@gmail.com>
2015-10-23 00:04:18 +0100

    Implemented getWorldType

--- src/com/ftwinston/KillerMinecraft/Modules/LavaLand/Plugin.java
+++ src/com/ftwinston/KillerMinecraft/Modules/LavaLand/Plugin.java
@@ -1,12 +1,16 @@
 package com.ftwinston.KillerMinecraft.Modules.LavaLand;
 
 import org.bukkit.Material;
+import org.bukkit.World.Environment;
 
 import com.ftwinston.KillerMinecraft.WorldGenerator;
 import com.ftwinston.KillerMinecraft.WorldGeneratorPlugin;
 
 public class Plugin extends WorldGeneratorPlugin
 {
+	@Override
+	public Environment getWorldType() { return Environment.NORMAL; }
+	
 	@Override
 	public String[] getDescriptionText() { return new String[] {"Add more lava to an", "otherwise-normal world"}; }
 	
